d723a9c0cdac432d591d9186075923eb554707fd
Imran Ahmed Younus <twinkleimran@users.noreply.github.com>
2017-03-28 00:15:43 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function(){
+
+    //bxslider_1
+
+$('.bxslider_1').bxSlider({
+      mode: 'horizontal',
+      auto:false,
+      pager:true,
+  });
+
+
+  //bxslider_2
+
+$('.bxslider_2').bxSlider({
+      mode: 'horizontal',
+      auto:true,
+      pager:false
+  });
+
+
+  //bxslider_3
+
+$('.bxslider_3').bxSlider({
+      mode: 'horizontal',
+      auto:false,
+      pager:false
+  });
+
+
+  //bxslider_4
+
+$('.bxslider_4').bxSlider({
+      mode: 'horizontal',
+      auto:false,
+      pager:false
+  });
+
+
+  //bxslider_5
+
+  $('.bxslider_5').bxSlider({
+      mode: 'fade',
+      auto:true,
+      pager:false
+  });
+
+
+
+});
